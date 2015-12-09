70df3e0c54d81c51dad4ffdad32da79f69e0f327
Robert Seidler <robert.seidler1@googlemail.com>
2015-12-09 06:24:08 +0100

    executorService in ReadInTask

--- src/de/rsp/wdntxml/fx/ReadInTask2.java
+++ src/de/rsp/wdntxml/fx/ReadInTask2.java
@@ -3,15 +3,28 @@ package de.rsp.wdntxml.fx;
 import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import de.rsp.wdntxml.langspec.WordnetParser;
 import de.rsp.wdntxml.structure.Wordnet;
 import de.rsp.wdntxml.tools.Output;
+import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyListProperty;
 import javafx.beans.property.ReadOnlyListWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 
+//TODO class needs a complete overhaul.
 public class ReadInTask2 extends Task<ObservableList<Wordnet>> {
 
 	private String dbUrl;
@@ -39,6 +52,10 @@ public class ReadInTask2 extends Task<ObservableList<Wordnet>> {
 	protected ReadOnlyListWrapper<Wordnet> partialResults = new ReadOnlyListWrapper<Wordnet>(this, "partialResults",
 			FXCollections.observableArrayList(new ArrayList<Wordnet>()));
 
+	private int progress = 0;
+	
+	private int maxProgress;
+	
 	public final ObservableList<Wordnet> getPartialResults() {
 
 		return partialResults.get();
@@ -63,6 +80,7 @@ public class ReadInTask2 extends Task<ObservableList<Wordnet>> {
 		this.sshPassword = sshPassword;
 		this.sshPort = sshPort;
 
+		maxProgress = inList.size();
 	}
 
 	public ReadInTask2(ObservableList<File> inList, WordnetParser chosenParser, boolean exportXml) {
@@ -72,46 +90,60 @@ public class ReadInTask2 extends Task<ObservableList<Wordnet>> {
 		this.inList = inList;
 		this.chosenParser = chosenParser;
 	}
-
+	
 	@Override
 	protected ObservableList<Wordnet> call() throws Exception {
 
-		updateProgress(0, 1);
-
+		// using executorService to ensure that database upload is a background
+		// progress, so that all the parsing and comparing is done, while
+		// database upload is probably still running. only supposed to run one
+		// uploading process at a time, the
+		// rest is in queue.
+		ExecutorService executor = new ThreadPoolExecutor(1, 1, 10, TimeUnit.HOURS,
+				new ArrayBlockingQueue<Runnable>(inList.size(), true));
+	
 		for (int i = 0; i < inList.size(); i++) {
-			
-			System.out.println("durschlauf " + i);
-			
+
 			WordnetParser rspParser = chosenParser.getClass().newInstance();
 			rspParser.setSourcePath(inList.get(i).getAbsolutePath());
 
 			Wordnet wordnet = rspParser.callCall();
-			System.out.println("ok 95");
 			// System.out.println(getProgress() + (1.0 / (double)
 			// inList.size()) + " / " + "1" + "\n");
-//			updateProgress(1/i, 1); //TODO reimplement progressbar
+			// updateProgress(1/i, 1); //TODO reimplement progressbar
 			// Platform.runLater(new Runnable() {
 			//
 			// @Override
 			// public void run() {
-			System.out.println("ok 103");
 			partialResults.get().add(wordnet);
+
 			if (exportXml)
 				Output.outToFileXmlFile(wordnet.getSynsetList(), wordnet.getLang(),
 						"D:\\Programmierung\\git repo\\wdntxml\\out");
-			try {
-				if (outData)
-					Output.outToDB(wordnet, dbUrl, dbUser, dbPassword, dbPort, sshUser, sshPassword, sshPort);
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("ok 118");
-		}
 
+			if (outData) {
+				// add database-upload-process to executorService
+				executor.submit(new Runnable() {
+
+					@Override
+					public void run() {
+
+						try {
+							Output.outToDB(wordnet, dbUrl, dbUser, dbPassword, dbPort, sshUser, sshPassword, sshPort);
+							progress++;
+							updateProgress(((double)progress)/((double)maxProgress), 1);
+							
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		}
 		return getPartialResults();
 	}
 
